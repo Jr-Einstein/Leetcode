68192bdfd2e4533f1107a01bc35aad597b1df373
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-06-11 04:29:04 +0530

    [189] [Time Beats: 78.01%] [Memory Beats: 39.11%] - LeetPush

--- 189-Rotate-Array.cpp
+++ 189-Rotate-Array.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;  // Ensure k is within the bounds of the array size
-        if (k == 0) return;  // If k is 0, no rotation is needed
-
+        k = k % n;
+        if (k == 0) return; 
         int temp[k];
-        // Store the first k elements
         for (int i = 0; i < k; i++) {
             temp[i] = nums[n - k + i];
         }
-        // Shift the rest of the array to the left by k positions
         for (int i = n - k - 1; i >= 0; i--) {
             nums[i + k] = nums[i];
         }
-        // Copy the first k elements to the beginning of the array
         for (int i = 0; i < k; i++) {
             nums[i] = temp[i];
         }
