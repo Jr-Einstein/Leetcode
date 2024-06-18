28d5805eb061b865c822eb5ddae7da182b954fc0
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-06-18 10:58:04 +0530

    [283] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        for(int j=0;j<nums.size();j++){
-            if(nums[j] !=0){
-                swap(nums[j],nums[i]);
-                i++;
+        int n=nums.size();
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
+        if(j==-1) return;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+
     }
 };
